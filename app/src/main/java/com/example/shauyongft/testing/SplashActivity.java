f8f15995a610b11d8b399ad02410d3ad6153f4b0
Shau Yong Ang <ShauYongFT@FT.local>
2015-11-16 17:02:42 +1000

    Initial commit

---
+++ app/src/main/java/com/example/shauyongft/testing/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.shauyongft.testing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ShauYongFT on 16/11/2015.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
